f64c5e4afa926ad50cf75250f67aa28d0249bd66
Bennet0708 <klepschbennet1@gmail.com>
2026-04-05 16:53:29 +0200

    Add files via upload

--- TerminWebsite.py
+++ TerminWebsite.py
@@ -1,8 +1,4 @@
 import streamlit as st
-supabase = create_client(
-    st.secrets["SUPABASE_URL"],
-    st.secrets["SUPABASE_KEY"]
-)
 from supabase import create_client
 from datetime import date, datetime, timedelta
 import pandas as pd
